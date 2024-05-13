14e26c0b75025be4038f57d3f44a2b404c5ca6bd
P-Serg <163842794+P-Serg@users.noreply.github.com>
2024-05-14 00:47:04 +0500

    Lesson_PS04

---
+++ main1.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+import time
+
+# browser = webdriver.Firefox()
+# browser.get("https://en.wikipedia.org/wiki/Document_Object_Model")
+# time.sleep(5)
+# browser.save_screenshot("dom.png")
+# browser.get("https://ru.wikipedia.org/wiki/Selenium")
+# browser.save_screenshot("selenium.png")
+# time.sleep(3)
+# browser.refresh()
+# browser.quit()
+
